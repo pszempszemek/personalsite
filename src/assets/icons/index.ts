f2a3acb8ae87c6b40bb6b0cf97c3a879efb004a6
pszempszemek <jestempszemek@gmail.com>
2023-03-07 20:38:28 +0100

    Contact Page filled

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,5 @@
 export { ReactComponent as GithubSvg } from "./github_logo.svg";
 export { ReactComponent as InstagramSvg } from "./instagram_logo.svg";
 export { ReactComponent as LinkedinSvg } from "./linkedin_logo.svg";
+export { ReactComponent as PhoneSvg } from "./phone.svg";
+export { ReactComponent as EmailSvg } from "./envelope.svg";
--- src/pages/Contact.tsx/Contact.tsx
+++ src/pages/Contact.tsx/Contact.tsx
@@ -1,8 +1,37 @@
 import React from "react";
 import styles from "./Contact.module.scss";
+import { PhoneSvg, EmailSvg } from "../../assets/icons";
+import ContactIcon from "../../components/ContactIcon/ContactIcon";
+
+const options = [
+  {
+    id: "Phone",
+    Svg: <PhoneSvg />,
+    note: "+48 725 062 338",
+  },
+  {
+    id: "Email",
+    Svg: <EmailSvg />,
+    note: "p.greczka96@gmail.com",
+  },
+];
 
 const Contact = () => {
-  return <div className={styles.container}>Contact</div>;
+  return (
+    <div className={styles.container}>
+      <h1>Contact Me</h1>
+      <p>
+        I am curently open to collaboration & freelance jobs. Please feel free
+        to contact using options from below:
+      </p>
+      <div className={styles.optionsContainer}>
+        {options.map((option) => (
+          <ContactIcon key={option.id} social={option} />
+        ))}
+      </div>
+      <p>or message me on socials shown at the bottom</p>
+    </div>
+  );
 };
 
 export default Contact;
